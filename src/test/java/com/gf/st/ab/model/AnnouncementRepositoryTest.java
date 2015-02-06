bd8c036afb7d578a3247ca434dc84a96c7c043d9
Rashidi Zin <rashidi@zin.my>
2015-02-06 16:48:07 +0800

    Add Announcement entity
    Initial commit to ensure that database configuration is working.
    
    Fixes gh-1

---
+++ src/test/java/com/gf/st/ab/model/AnnouncementRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.gf.st.ab.model;
+
+import com.gf.st.ab.AnnouncementBoardApplicationTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Rashidi Zin
+ */
+public class AnnouncementRepositoryTest extends AnnouncementBoardApplicationTests {
+
+    @Autowired
+    AnnouncementRepository $;
+
+    final Pageable pageable = new PageRequest(0, 1);
+    Announcement announcement = new Announcement("Integration Test", "Executing integration test!");
+
+    @Before
+    public void init() {
+        announcement = $.save(announcement);
+    }
+
+    @Test
+    public void findAllByCreated() {
+        assertTrue(
+                $.findAllByCreated(announcement.getCreated(), pageable)
+                        .hasContent()
+        );
+    }
+
+    @Test
+    public void findAllByMessageContaining() {
+        assertTrue(
+                $.findAllByMessageContaining(announcement.getMessage().substring(0, 10), pageable)
+                        .hasContent()
+        );
+    }
+
+    @After
+    public void delete() {
+        $.delete(announcement.getId());
+    }
+}
