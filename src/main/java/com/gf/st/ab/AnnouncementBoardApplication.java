f4ef4820dbd4ac2908191175509495c502c50c1d
Rashidi Zin <rashidi@zin.my>
2015-02-23 17:53:28 +0800

    Add hibernate-validator as dependency
    Hibernate validator provide the ease in validating fields with annotations such as @NotEmpty, @Email, etc.
    
    gh-9

--- src/main/java/com/gf/st/ab/AnnouncementBoardApplication.java
+++ src/main/java/com/gf/st/ab/AnnouncementBoardApplication.java
@@ -2,11 +2,24 @@ package com.gf.st.ab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class AnnouncementBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnnouncementBoardApplication.class, args);
+    }
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
     }
 }
