4f742dd0c7e1cef2e701ba852157c4514572c8fc
Rashidi Zin <rashidi@zin.my>
2015-03-03 17:49:59 +0800

    Implement security measures
    In order to perform any operations client is required to provide username and authorization token. Except for creating a user and accessing /login. Once a client has been validated at /login a token will be provided by the server and will be used by the client to inform the server that he is who he claimed to be.
    
    Should the token does not match with the username the client will not be able to perform any logged in operations.
    
    Fixes gh-12
    
    See gh-11

--- src/main/java/com/gf/st/ab/AnnouncementBoardApplication.java
+++ src/main/java/com/gf/st/ab/AnnouncementBoardApplication.java
@@ -1,12 +1,25 @@
 package com.gf.st.ab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import static org.springframework.boot.autoconfigure.security.SecurityProperties.ACCESS_OVERRIDE_ORDER;
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class AnnouncementBoardApplication {
 
     public static void main(String[] args) {
@@ -21,5 +34,33 @@ public class AnnouncementBoardApplication {
     @Bean
     public LocalValidatorFactoryBean validator() {
         return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
+    }
+
+    @Order(ACCESS_OVERRIDE_ORDER)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable().httpBasic();
+        }
+    }
+
+    @Order(HIGHEST_PRECEDENCE)
+    @Configuration
+    protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+
+        @Autowired
+        private UserDetailsService userDetailsService;
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService);
+        }
+
     }
 }
--- src/test/java/com/gf/st/ab/AnnouncementBoardApplicationTests.java
+++ src/test/java/com/gf/st/ab/AnnouncementBoardApplicationTests.java
@@ -1,18 +1,106 @@
 package com.gf.st.ab;
 
+import com.gf.st.ab.domain.Authorization;
+import com.gf.st.ab.domain.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static com.gf.st.ab.domain.UserStatus.ACTIVE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.http.HttpStatus.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AnnouncementBoardApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:0")
+@DirtiesContext
 public class AnnouncementBoardApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Value("${local.server.port}")
+	private int port;
+
+	private String url;
+	private TestRestTemplate restTemplate;
+	private Authorization authorization;
+
+	private User user = new User("morpheus@nebuchadnezzar.com", "morpheus", "ey3|348th31");
+
+	@Before
+	public void init() {
+		url = String.format("http://localhost:%d", this.port);
+
+		createUser();
+		authorize();
+
+		restTemplate = new TestRestTemplate(authorization.getUsername(), authorization.getToken());
 	}
 
+	@Test
+	public void getUser() {
+		ResponseEntity<User> response = restTemplate.getForEntity(String.format("%s/users/%s", url, user.getId()), User.class);
+
+		assertEquals(OK, response.getStatusCode());
+		assertEquals(user.getUsername(), response.getBody().getUsername());
+		assertEquals(user.getEmail(), response.getBody().getEmail());
+	}
+
+	@Test
+	public void getNonExistingUser() {
+		ResponseEntity<User> response = restTemplate.getForEntity(url + "/users/50m3r480MnUm8eR", User.class);
+
+		assertEquals(NOT_FOUND, response.getStatusCode());
+	}
+
+	@After
+	public void delete() {
+		deleteUser();
+		logout();
+	}
+
+	private void createUser() {
+		ResponseEntity<User> response = new TestRestTemplate().postForEntity(url + "/users", user, User.class);
+
+		assertEquals(CREATED, response.getStatusCode());
+		assertEquals(ACTIVE, response.getBody().getStatus());
+
+		user.setId(response.getBody().getId());
+	}
+
+	private void authorize() {
+		ResponseEntity<Authorization> response = new TestRestTemplate().postForEntity(url + "/authorization/login", user, Authorization.class);
+
+		assertEquals(OK, response.getStatusCode());
+
+		authorization = response.getBody();
+
+		assertNotNull(authorization.getUsername());
+		assertNotNull(authorization.getToken());
+	}
+
+	private void deleteUser() {
+		String userUrl = String.format("%s/users/%s", url, user.getId());
+
+		restTemplate.delete(userUrl);
+
+		ResponseEntity<User> response = restTemplate.getForEntity(userUrl, User.class);
+
+		assertEquals(NOT_FOUND, response.getStatusCode());
+	}
+
+	private void logout() {
+		ResponseEntity response = restTemplate.postForEntity(url + "/authorization/logout", null, Authorization.class);
+
+		assertEquals(OK, response.getStatusCode());
+	}
 }
