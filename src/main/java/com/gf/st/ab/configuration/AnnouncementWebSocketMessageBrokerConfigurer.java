ff52b9b9f2fc4d279ac8d941fd47e11f419cbce2
Rashidi Zin <rashidi@zin.my>
2015-03-11 15:51:50 +0800

    Add message broker to handle Announcement
    The broker is responsible to listen to any new messages and inform the subscriber about recent announcement.

---
+++ src/main/java/com/gf/st/ab/configuration/AnnouncementWebSocketMessageBrokerConfigurer.java
@@ -0,0 +1,26 @@
+package com.gf.st.ab.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * @author Rashidi Zin
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class AnnouncementWebSocketMessageBrokerConfigurer extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/announce").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/announcements");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
