8f197d8aab96aceb5ce7a8bd4fff0dc45611a61b
Khoo Wei Hiong, GfK <WeiHiong.Khoo@gfk.com>
2015-04-15 18:41:29 +0800

    Remove this js file and use app.js instead to bootstrap Angularjs application

--- src/main/resources/static/js/announcement-app.js
+++
@@ -1,7 +0,0 @@
-angular.module("announcementBoardApp", [
-    "announcementBoardApp.controllers",
-    "announcementBoardApp.services"
-]);
-
-angular.module("announcementBoardApp.controllers", []);
-angular.module("announcementBoardApp.services", []);
